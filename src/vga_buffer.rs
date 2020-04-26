4af8e2370f2c85bd1b4ad7e677b4cebd721e9d0c
Daniel Bencic <d.bencic@protonmail.com>
2020-04-27 01:30:19 +0200

    changed git user

---
+++ src/vga_buffer.rs
@@ -0,0 +1,40 @@
+pub enum Color {
+    Black,
+    Blue,
+    Green,
+    Cyan,
+    Red,
+    Magenta,
+    Brown,
+    LightGray,
+    DarkGray,
+    LightBlue,
+    LightGreen,
+    LightCyan,
+    LightRed,
+    Pink,
+    Yellow,
+    White
+}
+
+struct ColorCode(u8);
+
+impl ColorCode {
+    fn new(foreground: Color, background: Color) -> ColorCode {
+        ColorCode((background as u8) << 4 | (foreground as u8))
+    }
+}
+
+struct ScreenChar {
+    ascii_character: u8,
+    color_code: ColorCode
+}
+
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_WIDTH: usize = 80;
+
+struct Buffer {
+    chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT]
+}
+
+struct 
